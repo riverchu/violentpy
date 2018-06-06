895c28bc0afae7b97502dfd15ef66dd1c0df9bab
riverchu <churiver@protonmail.com>
2018-06-06 02:18:36 -0400

    update scan

--- scan/nmapscan.py
+++ scan/nmapscan.py
@@ -1,15 +1,78 @@
 #!/usr/bin/env python3
 # coding=utf-8
-__author__="riverchu"
+__author__ = "riverchu"
 
 import nmap
+from .resolveIP import resolveIP
 
-def nmap_scan(tgtHost,tgtPort):
-    nmScan = nmap.PortScanner()
-    result = nmScan.scan(tgtHost,str(tgtPort))
-    if result['scan'].__contains__(tgtHost):
-        return nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+
+def ret_info(dic, attr):
+    if dic.__contains__(attr):
+        return dic[attr]
     else:
         return None
 
 
+def host_scan(tgt_host, tgt_port):
+    nm_scan = nmap.PortScanner()
+    result = nm_scan.scan(tgt_host, str(tgt_port))
+    try:
+        for ip in result['scan']:
+            ip_info = ret_info(result['scan'], ip)
+            # address = ret_info(ip_info, 'addresses')
+            # mac = ret_info(address, 'mac')
+            # vendor = ret_info(ip_info['vendor'], mac)
+            port_info = ip_info['tcp'][int(tgt_port)]
+            state = port_info['state']
+            name = port_info['name']
+            # return(ip,mac,vendor,int(tgtPort),name,state)
+            return {'ip': ip, 'port': int(tgt_port), 'name': name, 'state': state}
+    except Exception as e:
+        print(e)
+
+
+# 结构需更新
+def nmap_scan(tgt_host, tgt_port):
+    nm_scan = nmap.PortScanner()
+    # print(tgt_host, tgt_port)
+    result = nm_scan.scan(tgt_host, str(tgt_port))
+    # print(result)
+    try:
+        for ip in result['scan']:
+            ip_info = ret_info(result['scan'], ip)
+            # address = ret_info(ip_info, 'addresses')
+            # mac = ret_info(address, 'mac')
+            # vendor = ret_info(ip_info['vendor'], mac)
+            port_info = ip_info['tcp'][int(tgt_port)]
+            state = port_info['state']
+            name = port_info['name']
+            # yield(ip,mac,vendor,int(tgtPort),name,state)
+            # print({'ip': ip, 'port': int(tgt_port), 'name': name, 'state': state})
+            yield {'ip': ip, 'port': int(tgt_port), 'name': name, 'state': state}
+    except Exception as e:
+        print('[-] Scan Error:', e)
+
+
+def ssh_scan_onebyone(tgt_host, tgt_port):
+    tgt_host = str(tgt_host)
+    tgt_port = str(tgt_port)
+    for host in resolveIP(tgt_host):
+        ret = host_scan(host, tgt_port)
+        if host is None or host['state'] != 'open' or host['name'] != 'ssh':
+            continue
+        yield ret
+
+
+# host range, str port
+def ssh_scan_all(tgt_host, tgt_port):
+    tgt_host = str(tgt_host)
+    tgt_port = str(tgt_port)
+    for host in nmap_scan(tgt_host, tgt_port):
+        if host is None or host['state'] != 'open' or host['name'] != 'ssh':
+            continue
+        yield host
+
+
+if __name__ == '__main__':
+    for ret in nmap_scan('10.108.36.71/24', '22'):
+        print(ret)
--- scan/resolveIP.py
+++ scan/resolveIP.py
@@ -1,78 +1,85 @@
 #!/usr/bin/env python3
 # coding=utf-8
-__author__="riverchu"
+__author__ = "riverchu"
 
 from socket import *
 
+
 def dot2intIP(ip):
-	ip = (int(ip[0])<<24) + (int(ip[1])<<16) + (int(ip[2])<<8) + int(ip[3])
-	return ip
+    ip = (int(ip[0])<<24) + (int(ip[1])<<16) + (int(ip[2])<<8) + int(ip[3])
+    return ip
+
 
 def int2dotIP(intIP):
-	ip=[0,0,0,0]
-	ip[0]=intIP>>24;
-	ip[1]=(intIP>>16) & 255
-	ip[2]=(intIP>>8) & 255
-	ip[3]=(intIP) & 255
-	return str(ip[0])+'.'+str(ip[1])+'.'+str(ip[2])+'.'+str(ip[3])
+    ip=[0,0,0,0]
+    ip[0]=intIP>>24;
+    ip[1]=(intIP>>16) & 255
+    ip[2]=(intIP>>8) & 255
+    ip[3]=(intIP) & 255
+    return str(ip[0])+'.'+str(ip[1])+'.'+str(ip[2])+'.'+str(ip[3])
+
 
 def maskCalc(mask,ipl):
-	i = 0
-	for c in range(mask):
-		i = (i<<1)+1
-	i = i<<(8-mask)
+    i = 0
+    for c in range(mask):
+        i = (i<<1)+1
+    i = i<<(8-mask)
 
-	i = i&int(ipl)
+    i = i&int(ipl)
 
-	return str(i)+'-'+str(i+2**(8-mask))
+    return str(i)+'-'+str(i+2**(8-mask))
+
 
 #192.168.1.1-255
 def resolveRangeIP(ipL):
-	maxip = [0,0,0,0]
-	minip = [0,0,0,0]
+    maxip = [0,0,0,0]
+    minip = [0,0,0,0]
 
-	ip = ipL.split('.')
+    ip = ipL.split('.')
 
-	for x in range(4):
-		if ip[x].find('-')!=-1:
-			s,e = ip[x].split('-')
-			minip[x] = int(s)
-			maxip[x] = int(e)
-		else:
-			maxip[x] = int(ip[x])
-			minip[x] = int(ip[x])
+    for x in range(4):
+        if ip[x].find('-')!=-1:
+            s,e = ip[x].split('-')
+            minip[x] = int(s)
+            maxip[x] = int(e)
+        else:
+            maxip[x] = int(ip[x])
+            minip[x] = int(ip[x])
 
-	return dot2intIP(minip),dot2intIP(maxip)
+    return dot2intIP(minip),dot2intIP(maxip)
+
 
 #192.168.1.1-192.168.1.255
 def resolveRangeIPL(ipL):
-	minip,maxip = ipL.split('-')
-	minip = minip.split('.')
-	maxip = maxip.split('.')
-	return dot2intIP(minip),dot2intIP(maxip)
+    minip,maxip = ipL.split('-')
+    minip = minip.split('.')
+    maxip = maxip.split('.')
+    return dot2intIP(minip),dot2intIP(maxip)
+
 
 #192.168.1.1/24
 def resolveCIDR(ipL):
-	ip,mask=ipL.split('/')
+    ip,mask=ipL.split('/')
 
-	ip=ip.split('.')
-	mask = int(mask)
+    ip=ip.split('.')
+    mask = int(mask)
 
-	if mask<=8:
-		ranip = maskCalc(mask,ip[0])
-		return resolveRangeIP(ranip+'.0-255.0-255.0-255')
-	elif mask<=16:
-		ranip = maskCalc(mask-8,ip[1])
-		return resolveRangeIP(ip[0]+'.'+ranip+'.0-255.0-255')
-	elif mask<=24:
-		ranip = maskCalc(mask-16,ip[2])
-		return resolveRangeIP(ip[0]+'.'+ip[1]+'.'+ranip+'.0-255')
-	elif mask<=30:
-		ranip = maskCalc(mask-24,ip[3])
-		return resolveRangeIP(ip[0]+'.'+ip[1]+'.'+ip[2]+'.'+ranip)
-	else:
-		print('[-]Input wrong ip range.')
-		exit(0)
+    if mask<=8:
+        ranip = maskCalc(mask,ip[0])
+        return resolveRangeIP(ranip+'.0-255.0-255.0-255')
+    elif mask<=16:
+        ranip = maskCalc(mask-8,ip[1])
+        return resolveRangeIP(ip[0]+'.'+ranip+'.0-255.0-255')
+    elif mask<=24:
+        ranip = maskCalc(mask-16,ip[2])
+        return resolveRangeIP(ip[0]+'.'+ip[1]+'.'+ranip+'.0-255')
+    elif mask<=30:
+        ranip = maskCalc(mask-24,ip[3])
+        return resolveRangeIP(ip[0]+'.'+ip[1]+'.'+ip[2]+'.'+ranip)
+    else:
+        print('[-]Input wrong ip range.')
+        exit(0)
+
 
 def isIP(string):
     try:
@@ -106,6 +113,7 @@ def isIP(string):
     except:
         return 0
 
+
 def resolve_host(tgtHost):
     try:
         tgtIP = gethostbyname(tgtHost)
@@ -116,6 +124,7 @@ def resolve_host(tgtHost):
         return tgtName[0]
     except:
         return tgtIP
+
 
 def resolveIP(iprange):
     ipType = isIP(iprange)
@@ -131,6 +140,7 @@ def resolveIP(iprange):
 
     for ip in range(start,end+1):
         yield int2dotIP(ip)
+
 
 if __name__ == "__main__":
     print("[+] example:192.168.1.1/28")
