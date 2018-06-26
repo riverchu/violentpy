d14490269caafa08193b4c5c4385303ef500ff7e
riverchu <churiver@protonmail.com>
2018-06-26 22:32:33 +0800

    dic change

--- global_var.py
+++ global_var.py
@@ -13,6 +13,6 @@ OPERATE_INTERVAL = 3
 
 DATA_PATH = '/root/h/data/'
 SSH_DICTIONARY = DATA_PATH+'dictionary/common/online_brute.txt'
-PASS_DICTIONARY = DATA_PATH+'dictionary/common/top100thousand.txt'
+PASS_DICTIONARY = DATA_PATH+'dictionary/common/top1million.txt'
 BOT_PATH = DATA_PATH+'hData/bot/'
 bot_info_file = 'bot_'+time.strftime('%Y-%m-%d_%Hh')+'.txt'
